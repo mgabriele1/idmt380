80b02812357a82f3e4ff69187fe697c3028d855f
Hunter <hmh84@drexel.edu>
2020-07-06 15:05:52 -0400

    :art: Canvas Updates
    -Improved responsivness
    -Added color selector
    -Fixed swatch picker
    -Reordered colors
    -Added hover states

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var canvas = document.querySelector("canvas");
+var canvas = document.querySelector('canvas');
 
 if (canvas.getContext) {
-    var context = canvas.getContext("2d");
+    var context = canvas.getContext('2d');
     // drawing code here
     
     var pos = {x: 0, y: 0};
     
-    document.addEventListener("mouseenter", setPosition);
-    document.addEventListener("mousedown", setPosition);
-    document.addEventListener("mousemove", draw);
+    document.addEventListener('mouseenter', setPosition);
+    document.addEventListener('mousedown', setPosition);
+    document.addEventListener('mousemove', draw);
     
-    // window.addEventListener("resize", resizeCanvas);
+    // window.addEventListener('resize', resizeCanvas);
     // resizeCanvas();
     
     // // Resize Canvas
@@ -30,27 +30,52 @@ if (canvas.getContext) {
     }
     
     const swatches = document.querySelectorAll('.color');
+
     
     swatches.forEach(swatch => {
-        if (swatch.hasAttribute('id', 'selected')) {
-            swatch.removeAttribute('id');
-        }
         swatch.style.backgroundColor = swatch.dataset.hex;
+        // Sets background color of each .color element to hex dataset of itself
+
         swatch.addEventListener('click', () => {
+            // If clicked do the following
+
+            swatches.forEach(swatch => {
+                // For each .color element, check for id="selected" and remove it
+                if (swatch.hasAttribute('id')) {
+                    swatch.removeAttribute('id', 'selected');
+                }
+            });
             swatch.setAttribute('id', 'selected');
-        })
+            // Sets id of clicked .color element to "selected"
+
+            if (swatch.hasAttribute('type', 'color')) {
+                console.log('New Brush Color = ' + swatch.value);
+            } else {
+                console.log('New Brush Color = ' + swatch.dataset.hex);
+            };
+
+        });
     });
+
+    // Set default swatch
+    const default_swatch = document.querySelectorAll('.color')[0];
+    default_swatch.setAttribute('id', 'selected');
     
     function draw(e)  {
         if (e.buttons !== 1) return; 
         // Stop if mouse is not clicked
-        context.lineWidth = 20;
-        context.lineCap = "round";
-        
-        var selected_swatch = document.querySelector('#selected').dataset.hex;
-        console.log(selected_swatch);
-        
-        context.strokeStyle = selected_swatch;
+        context.lineWidth = 5;
+        context.lineCap = 'round';
+
+        // Find element with id="selected" then set the brush color to its hex dataset
+        var selected_swatch = document.querySelector('#selected');
+
+        // Check if the element selected was color picker or swatch and gets value
+        if (selected_swatch.hasAttribute('type', 'color')) {
+            context.strokeStyle = selected_swatch.value;
+        } else {
+            context.strokeStyle = selected_swatch.dataset.hex;
+        }
         
         setPosition(e);
         
@@ -62,8 +87,33 @@ if (canvas.getContext) {
         // To position
         context.stroke();
         // Draw the stroke
-    }
+    };
 
+    // Buttons
+
+const btn_trash = document.querySelector('#btn-trash');
+btn_trash.addEventListener('click', () => {
+    location.reload();
+});
+
+const color_wheel = document.querySelector('.color-wheel');
+const color_input = document.querySelector('input[type="color"]');
+
+color_wheel.addEventListener('click', () => {
+    color_input.click();
+});
+
+window.addEventListener('resize', move_wheel);
+window.onload = function() {
+    setTimeout(() => {
+        move_wheel();
+    }, 1);
+};
+
+function move_wheel() {
+    color_wheel.style.top = color_input.offsetTop;
+    color_wheel.style.left = color_input.offsetLeft;
+};
 
     } else {
         alert('Canvas is not supported in your browser.')
