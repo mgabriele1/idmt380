e63240c5aadf6843b574dc825d3fa8e12fb3adc4
Hunter Hampton <hmh84@drexel.edu>
2020-07-28 12:12:42 -0400

    Fill Tool
    -Removed all tools except fill
    -Changed canvas to svg
    -Changed swatch color form rgb to hex

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var canvas = document.querySelector('#canvas');
 
 if (canvas.getContext) {
@@ -63,9 +64,16 @@ if (canvas.getContext) {
     // Set default swatch
     const default_swatch = document.querySelectorAll('.colors')[0];
     default_swatch.setAttribute('id', 'selected');
+=======
+const color_picker = document.querySelector('input[type="color"]');
+
+// Color Wheel -> Picker
+const color_wheel = document.querySelector('.stack');
+>>>>>>> Stashed changes
 
 canvas_wrap = document.querySelector('#canvas-wrap');
 
+<<<<<<< Updated upstream
 resizeWindow();
 
 // Default position
@@ -175,3 +183,62 @@ function move_wheel() {
     } else {
         alert('Canvas is not supported in your browser.');
     };
+=======
+// Swatches
+const swatches = document.querySelectorAll('.swatch');
+
+swatches.forEach(swatch => {
+    swatch.style.backgroundColor = swatch.dataset.swatch;
+    swatch.setAttribute('title', swatch.dataset.swatch);
+    
+    // Color Mode = Picker
+    swatch.addEventListener('click', () => {
+        color_mode = 'swatch';
+        // Set the new swatch
+        new_swatch = swatch;
+        choose_color();
+    })
+});
+
+// Color Mode = Picker
+color_picker.addEventListener('click', () => {
+    color_mode = 'picker';
+    choose_color();
+});
+
+// Init color mode, set picker value to value of first swatch
+var color_mode = 'picker';
+color_picker.setAttribute('value', swatches[0].dataset.swatch)
+choose_color();
+
+// Determine Color
+function choose_color() {
+    console.log('Color Mode = ' + color_mode);
+
+    if (color_mode == 'picker') {
+        color = color_picker.value;
+    } else if (color_mode == 'swatch') {
+        color = new_swatch.dataset.swatch;
+    }
+
+    console.log('Paint Color = ' + color);
+}
+
+// Fill Tool
+$('svg').click(function(event) {
+    event.target.style.fill = color;
+});
+
+// Download Artwork
+const download_btn = document.querySelector('[data-command="download"]');
+
+download_btn.addEventListener('click', () => {
+    const svg = document.querySelector(".main-area").innerHTML;
+    const blob = new Blob([svg.toString()]);
+    const element = document.createElement("a");
+    element.download = "My-Coloring-Book.svg";
+    element.href = window.URL.createObjectURL(blob);
+    element.click();
+    element.remove();
+});
+>>>>>>> Stashed changes
