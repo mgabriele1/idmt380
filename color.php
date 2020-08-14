042fd7896c0d36658a6bfcf966477018f6128d7c
Victoria Stauffer <vstauffer21@gmail.com>
2020-08-14 10:13:47 -0400

    CSS for color page nearly done. Still needs responsiveness in the bottom color-selection so they stay circles.

--- color.php
+++ color.php
@@ -18,7 +18,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>WIP</title>
         <link rel="stylesheet" href="dist/css/normalize.css">
-        <link rel="stylesheet" href="dist/css/main.css">
+        <link rel="stylesheet" href="css/main.css">
         <script
             src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
@@ -29,7 +29,7 @@
 <body class="coloring-page">
 <div id="logo"> <a href="index.php"><img src="graphics/logo.png"> </a></div>
     <main class="coloring-page" id="colorpage">
-        <div class="aside">
+        <div class="aside" id="top-aside">
             <div class="group commands">
                 <div class="item command" data-command="undo"><img class="svg" src="graphics/undo.svg" alt="undo"></div>
                 <div class="item command" data-command="download"><img class="svg" src="graphics/download.svg" alt="download"></div>
@@ -41,6 +41,7 @@
             </div>
         </div>
         <div class="main-area">
+            <img src="graphics/artwork/DIGM223_An_Christina.svg">
             <?php include_once 'graphics/artwork/'.$row['image']; ?>
         </div>
         <div class="aside">
