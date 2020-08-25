61727afdcc929b158969392049f464ab2d4127bd
Hunter Hampton <hmh84@drexel.edu>
2020-08-24 23:44:05 -0400

    Rename
    * Renamed WIP to Paint

--- color.php
+++ color.php
@@ -17,7 +17,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>WIP</title>
+        <title>Paint</title>
         <link rel="stylesheet" href="dist/css/normalize.css">
         <link rel="stylesheet" href="dist/css/main.css">
         <script
