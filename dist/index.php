dd183c911f3578df9ea44ac880e07d72b88f1e2c
Hunter Hampton <hmh84@drexel.edu>
2020-08-31 17:30:52 -0400

    Gulp

--- dist/index.php
+++ dist/index.php
@@ -1,6 +1,6 @@
 <?php
     $current_page = 'index';
-    include_once 'includes/head.php';
+    include_once 'dist/includes/head.php';
 ?> <div class="modal" style="display: <?php if (!isset($_GET['upload'])) {echo 'none';} else {echo 'flex';} ?>"><div class="upload-success"><h1>Upload successful!</h1><p>Your artwork is now featured at the top.</p><button id="close-modal">Close</button></div></div><!--tab switching--><div class="tabs"><a href="index.php"><div class="tab <?php 
             if (isset($_GET['tab'])) {
 
@@ -14,7 +14,7 @@
         ?> " id="tab2"><p>User Colored</p></div></a></div><div class="image-grid user-created"> <?php
             if (isset($_GET['tab'])) {
                 $table = 'community';
-                include_once 'includes/record_handler.php';
+                include_once 'dist/includes/record_handler.php';
                 // $the_echo = include_once 'graphics/.'$table'./'.$row['image'];
                 $sql = "SELECT * FROM $table ORDER BY timestamp DESC LIMIT 100;";
             } else {
@@ -38,4 +38,4 @@
             } else {
                 echo "No artwork to show!";
             }
-            ?> <a class="back-to-top" href="#top"><img class="b2t" src="graphics/btt.svg" alt="back to top"></a></div> <?php include_once 'include/footer.php'; ?>
+            ?> <a class="back-to-top" href="#top"><img class="b2t" src="graphics/btt.svg" alt="back to top"></a></div> <?php include_once 'dist/include/footer.php'; ?>
